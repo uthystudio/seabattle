72d37e21147eca0ccc6784c299714210b86fa73f
feironox5 <feironox5@gmail.com>
2019-08-12 10:58:16 +0300

    Killed/Shot Messages

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -59,15 +59,28 @@ export class BoardComponent implements OnInit {
       return Math.floor(Math.random() * (max - min)) + min;
     }
     let i: number;
-    const a = getRandomInt(0, 7);
-    const b = getRandomInt(0, 7);
+    let a: number;
+    let b: number;
+    const help = String(cellsToShot[Math.floor(Math.random() * cellsToShot.length)] + 1);
+    if (help.length === 1) {
+      a = 0;
+      b = Number(help);
+    } else {
+      a = Number(help.slice(0, 1));
+      b = Number(help.slice(1, 2));
+    }
     const rot = getRandomInt(1, 3);
     const helpArray = [...Array(4).keys()];
+    let toGo: string;
     for (i of helpArray) {
       if (rot === 1) {
         this.setShip(a, b + i, idShip);
-        const ab = String(a) + String(b + i);
-        delete cellsToShot[Number(ab) - 1];
+        if (a === 0) {
+          toGo = String(b);
+        } else {
+          toGo = String(a) + String(b + i);
+        }
+        delete cellsToShot[Number(toGo) - 1];
       } else {
         this.setShip(a + i, b, idShip);
       }
